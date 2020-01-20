61c65a40c1f77b3ac3aaf6e9eda59d95c98ffbea
Hermann Hueck <hermann.hueck@mailbox.org>
2020-01-20 18:49:45 +0100

    scalactic gettingstarted

---
+++ core/src/test/scala/scalactic/gettingstarted/GettingStarted.scala
@@ -0,0 +1,28 @@
+package scalactic.gettingstarted
+
+object GettingStarted {
+
+  import org.scalactic._
+
+  {
+    import TypeCheckedTripleEquals._
+
+    // Some(1) === 2
+    Some(1) === Some(2)
+    // 1 === 1L
+  }
+
+  /*
+    import ConversionCheckedTripleEquals._
+
+    1 === 1L
+    List(1, 2, 3) === Vector(1, 2, 3)
+   */
+
+  {
+    // import TraversableEqualityConstraints._
+
+    // List(1, 2, 3) === Vector(1, 2, 3)
+    // List(1, 2, 3) === Set(1, 2, 3)
+  }
+}
