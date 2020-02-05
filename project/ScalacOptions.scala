5c6902a5e32713084aa64db38262749054061800
Hermann Hueck <hermann.hueck@mailbox.org>
2020-02-06 00:25:59 +0100

    commented scalacOption -Ywarn-unused:imports

--- project/ScalacOptions.scala
+++ project/ScalacOptions.scala
@@ -18,12 +18,12 @@ object ScalacOptions {
     "-Ywarn-extra-implicit",     // Warn when more than one implicit parameter section is defined.
     "-Ywarn-numeric-widen",      // Warn when numerics are widened.
     "-Ywarn-unused:implicits",   // Warn if an implicit parameter is unused.
-    "-Ywarn-unused:imports",     // Warn if an import selector is not referenced.
-    "-Ywarn-unused:locals",      // Warn if a local definition is unused.
-    "-Ywarn-unused:params",      // Warn if a value parameter is unused.
-    "-Ywarn-unused:patvars",     // Warn if a variable bound in a pattern is unused.
-    "-Ywarn-unused:privates",    // Warn if a private member is unused.
-    "-Ywarn-value-discard",      // Warn when non-Unit expression results are unused.
+    // "-Ywarn-unused:imports",     // Warn if an import selector is not referenced.
+    "-Ywarn-unused:locals",   // Warn if a local definition is unused.
+    "-Ywarn-unused:params",   // Warn if a value parameter is unused.
+    "-Ywarn-unused:patvars",  // Warn if a variable bound in a pattern is unused.
+    "-Ywarn-unused:privates", // Warn if a private member is unused.
+    "-Ywarn-value-discard",   // Warn when non-Unit expression results are unused.
     "-Ybackend-parallelism",
     "4",                                         // Enable paralellisation — change to desired number!
     "-Ycache-plugin-class-loader:last-modified", // Enables caching of classloaders for compiler plugins
@@ -58,8 +58,9 @@ object ScalacOptions {
   }
 
   def removeScalacOptionXlintUnusedForConsoleFrom(scalacOptions: Seq[String]): Seq[String] =
-    scalacOptions.map {
-      case "-Xlint" => "-Xlint:-unused,_"
-      case other    => other
-    }
+    scalacOptions
+      .map {
+        case "-Xlint" => "-Xlint:-unused,_"
+        case other    => other
+      }
 }
